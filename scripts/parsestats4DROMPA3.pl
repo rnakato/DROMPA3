ff0e78073efab5fa1621d82c0502edb94429bf94
rnakato <rnakato@iam.u-tokyo.ac.jp>
2015-10-02 12:55:32 +0900

    modify parsestats4DROMPA3.pl

--- scripts/parsestats4DROMPA3.pl
+++ scripts/parsestats4DROMPA3.pl
@@ -20,8 +20,6 @@ while(<File>){
     }elsif($_ =~ /Library complexity: (.+) \((.+)\)/){
 	$tested_complexity = $1;
 	$tested_reads = $2;
-    }elsif($_ =~ /FRiP: (.+)/){
-	$FRiP = $1;
     }elsif($_ =~ /GC summit: (.+)/){
 	$gcsummit = $1;
     }elsif($_ =~ /Whole genome/){
@@ -35,14 +33,22 @@ while(<File>){
 	if($gcsummit eq ""){
 	    $depth = $clm[14];
 	    $total_gc_base = $clm[17];
+	    $FRiP = $clm[18] if($#clm >= 18);
 	}else{
 	    $depth = $clm[17];
 	    $total_gc_base = $clm[20];
+	    $FRiP = $clm[21] if($#clm >= 21);
 	}
     }
 }
 close (File);
 
-print STDERR "Sample\tMapped reads\t + strand\t - strand\tRedundancy threshold\tNonredundant\tRedundant\tComplexity for10M\tRead depth\tFRiP\tGenome coverage\tTested_reads\tGC summit\n";
+print STDERR "Sample\tMapped reads\t + strand\t - strand\tRedundancy threshold\tNonredundant\tRedundant\tComplexity for10M\tRead depth\tGenome coverage\tTested_reads";
+print STDERR "\tGC summit" if($gcsummit ne "");
+print STDERR "\tFRiP" if($FRiP ne "");
+print "\n";
 
-print "$file\t$total_reads\t$plus\t$minus\t$pcrbiasthre\t$total_remained\t$total_filtered\t$tested_complexity\t$depth\t$FRiP\t$total_gc_base\t$tested_reads\t$gcsummit\n";
+print "$file\t$total_reads\t$plus\t$minus\t$pcrbiasthre\t$total_remained\t$total_filtered\t$tested_complexity\t$depth\t$total_gc_base\t$tested_reads";
+print "\t$gcsummit" if($gcsummit ne "");
+print "\t$FRiP" if($FRiP ne "");
+print "\n";
